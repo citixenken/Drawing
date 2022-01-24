7ac06e88677184b023978f7b340ba6e52aadbd04
citixenken <kmuyesu@gmail.com>
2022-01-24 22:54:10 +0300

    first commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ken Muyesu on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
